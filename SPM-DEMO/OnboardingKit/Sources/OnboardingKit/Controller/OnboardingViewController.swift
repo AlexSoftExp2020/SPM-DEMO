d16c97f9a2f7e507bbfe0aa7b453786572680a83
Alex <oleksii.pidhornyi@gmail.com>
2024-11-11 01:07:53 +0100

    add onboarding view controller

---
+++ SPM-DEMO/OnboardingKit/Sources/OnboardingKit/Controller/OnboardingViewController.swift
@@ -0,0 +1,15 @@
+//
+//  OnboardingViewController.swift
+//  OnboardingKit
+//
+//  Created by Oleksii on 10.11.2024.
+//
+
+import UIKit
+
+class OnboardingViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
--- SPM-DEMO/OnboardingKit/Sources/OnboardingKit/OnboardingKit.swift
+++ SPM-DEMO/OnboardingKit/Sources/OnboardingKit/OnboardingKit.swift
@@ -1,2 +1,20 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import UIKit
+
+public class OnboardingKit {
+    
+    @MainActor
+    private lazy var onboardingViewController: OnboardingViewController = {
+        let controller = OnboardingViewController()
+        controller.modalTransitionStyle = .crossDissolve
+        controller.modalPresentationStyle = .fullScreen
+        return controller
+    }()
+    
+    public init() {}
+    
+    @MainActor
+    public func launchOnboarding(rootVC: UIViewController) {
+        rootVC.present(onboardingViewController, animated: true)
+    }
+    public func dismissOnboarding() {}
+}
