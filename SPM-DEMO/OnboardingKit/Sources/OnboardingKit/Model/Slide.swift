80cf35191eee1abbadd3996d889d366b4e868b30
Alex <oleksii.pidhornyi@gmail.com>
2024-11-12 23:49:43 +0100

    add framework dependancy

---
+++ SPM-DEMO/OnboardingKit/Sources/OnboardingKit/Model/Slide.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  OnboardingKit
+//
+//  Created by Oleksii on 12.11.2024.
+//
+
+import UIKit
+
+public struct Slide {
+    let image: UIImage
+    let title: String
+    
+    public init(image: UIImage, title: String) {
+        self.image = image
+        self.title = title
+    }
+}
--- SPM-DEMO/OnboardingKit/Sources/OnboardingKit/OnboardingKit.swift
+++ SPM-DEMO/OnboardingKit/Sources/OnboardingKit/OnboardingKit.swift
@@ -2,15 +2,21 @@ import UIKit
 
 public class OnboardingKit {
     
+    private let slides: [Slide]
+    private let tintColor: UIColor
+    
     @MainActor
     private lazy var onboardingViewController: OnboardingViewController = {
-        let controller = OnboardingViewController()
+        let controller = OnboardingViewController(slides: slides, tintColor: tintColor)
         controller.modalTransitionStyle = .crossDissolve
         controller.modalPresentationStyle = .fullScreen
         return controller
     }()
     
-    public init() {}
+    public init(slides: [Slide], tintColor: UIColor) {
+        self.slides = slides
+        self.tintColor = tintColor
+    }
     
     @MainActor
     public func launchOnboarding(rootVC: UIViewController) {
