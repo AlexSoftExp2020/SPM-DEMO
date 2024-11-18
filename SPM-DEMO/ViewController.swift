db73f1be37f72cc08ccd473db11d77cf3732305e
Alex <oleksii.pidhornyi@gmail.com>
2024-11-18 10:58:27 +0100

    handle get started button

--- SPM-DEMO/ViewController.swift
+++ SPM-DEMO/ViewController.swift
@@ -14,21 +14,18 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        onboardingKit = OnboardingKit(slides: [
-            .init(image: UIImage(named: "imSlide1")!, title: "Personalised offers at 40,000+ places"),
-            .init(image: UIImage(named: "imSlide2")!, title: "Stack your rewards every time you pay"),
-            .init(image: UIImage(named: "imSlide3")!, title: "Enjoy now, FavePay Later"),
-            .init(image: UIImage(named: "imSlide4")!, title: "Earn cashback with your physical card"),
-            .init(image: UIImage(named: "imSlide5")!, title: "Save and earn cashback with Deals or eCards")
-        ],
-                                      tintColor: UIColor(red: 220/255, green: 20/255, blue: 60/255, alpha: 1.0))
-        onboardingKit?.delegate = self
-        onboardingKit?.launchOnboarding(rootVC: self)
+        DispatchQueue.main.async {
+            self.onboardingKit = OnboardingKit(slides: [
+                .init(image: UIImage(named: "imSlide1")!, title: "Personalised offers at 40,000+ places"),
+                .init(image: UIImage(named: "imSlide2")!, title: "Stack your rewards every time you pay"),
+                .init(image: UIImage(named: "imSlide3")!, title: "Enjoy now, FavePay Later"),
+                .init(image: UIImage(named: "imSlide4")!, title: "Earn cashback with your physical card"),
+                .init(image: UIImage(named: "imSlide5")!, title: "Save and earn cashback with Deals or eCards")
+            ],
+                                          tintColor: UIColor(red: 220/255, green: 20/255, blue: 60/255, alpha: 1.0))
+            self.onboardingKit?.delegate = self
+            self.onboardingKit?.launchOnboarding(rootVC: self)
+        }
     }
 }
 
@@ -38,7 +35,47 @@ extension ViewController: OnboardingKitDelegate {
     }
     
     func getStartedButtonDidTap() {
-        print("")
+        onboardingKit?.dismissOnboarding()
+        onboardingKit = nil
+        transit(viewController: MainViewController())
+    }
+    
+    private func transit(viewController: UIViewController) {
+        let foregroundScenes = UIApplication
+            .shared
+            .connectedScenes
+            .filter { $0.activationState == .foregroundActive }
+        
+        let window = foregroundScenes
+            .map { $0 as? UIWindowScene }
+            .compactMap { $0 }
+            .first?
+            .windows
+            .filter({ $0.isKeyWindow })
+            .first
+        
+        guard let window else {
+            return
+        }
+        window.rootViewController = viewController
+        
+        UIView.transition(with: window,
+                          duration: 0.3,
+                          options: [.transitionCrossDissolve],
+                          animations: nil,
+                          completion: nil)
+    }
+}
+
+class MainViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let label = UILabel()
+        label.text = "Main View Controller"
+        view.addSubview(label)
+        label.snp.makeConstraints { make in
+            make.center.equalTo(view)
+        }
     }
 }
 
